76fc6e0de119d08050dd3df6914e92c8255ce34b
joaocoutod <joaocoutod@gmail.com>
2022-10-29 16:36:41 -0400

    att

--- app/conexao/Conexao.php
+++ app/conexao/Conexao.php
@@ -2,11 +2,7 @@
 class Conexao {
    
    public static $instance;
-
-   private function __construct() {
-       //
-   }
-
+    
    public static function getConexao() {
        if (!isset(self::$instance)) {
            self::$instance = new PDO('mysql:host=localhost;dbname=projetophp_joaolucas', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
